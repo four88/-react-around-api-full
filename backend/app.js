c51c519e040dcbb90261b6c5c7e5bc51426c091e
Pharanyu <pharanyuchuenjit@gmail.com>
2023-06-28 22:43:49 +0700

    edit cors

--- backend/app.js
+++ backend/app.js
@@ -97,9 +97,9 @@ app.post(
   '/signin',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().custom(validateURL),
+      // name: Joi.string().min(2).max(30),
+      // about: Joi.string().min(2).max(30),
+      // avatar: Joi.string().custom(validateURL),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
